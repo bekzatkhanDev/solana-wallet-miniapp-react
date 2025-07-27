2bb838e4a33fda8e25792d30e66235668d35c1d9
bekzatkhanDev <bekzatkhan.zhumabekov@gmail.com>
2025-07-27 15:22:37 +0500

    installed solana lib and made local adress creator function

--- src/App.js
+++ src/App.js
@@ -1,9 +1,12 @@
 import './App.css';
-import { useEffect } from 'react';
-
+import { useEffect, useState } from 'react';
+import * as solanaWeb3 from "@solana/web3.js";
+import { generateKeyPairSigner } from "@solana/kit";
 const tg = window.Telegram.WebApp;
 
 function App() {
+  const [wallet, setWallet] = useState([]);
+
   useEffect(()=> {
     tg.ready();
   }, [])
@@ -11,10 +14,17 @@ function App() {
   const onClose = () => {
     tg.close()
   }
+
+  const handleCreate = async () => {
+    const keypairSigner = await generateKeyPairSigner();
+    setWallet([keypairSigner.address,keypairSigner.keyPair.privateKey ])
+  }
   return (
     <div className="App">
         <button onClick={onClose}> Close</button>
         <span>{tg.initDataUnsafe?.user?.first_name}</span>
+        <button onClick={handleCreate}> Create address</button>
+        <div> {wallet}</div>
     </div>
   );
 }
